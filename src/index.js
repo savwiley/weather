7ca669dcb813ba73053e17b0c23564c9630c7efd
Savannaha <savwiley@yahoo.com>
2021-01-28 11:26:51 -0600

    added API notes

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ const contents = document.querySelector(".contents");
 
 contents.style.background = "green";
 contents.textContent = "I'm working!";
+
+
+/*
+OpenWeather API
+
+a649f8f0f6c898c4fe7ae2dfea5f0800
+name: weather-app
+
+https://openweathermap.org/current
+*/
